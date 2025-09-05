428ddb55dd8dfa952f5241254ac52d1c29a09b2a
Serhii Taran <serg.taran1970@gmail.com>
2025-09-05 23:11:34 +0300

    add-full-slot-machine

--- src/js/6-slot-machine.js
+++ src/js/6-slot-machine.js
@@ -5,6 +5,9 @@ const result = document.querySelector('.result');
 startBtn.addEventListener('click', handleStart);
 
 function handleStart() {
+  startBtn.disabled = true;
+  result.innerHTML = '';
+
   const promises = [...container.children].map(() => {
     return new Promise((resolve, reject) => {
       const random = Math.random();
@@ -18,9 +21,20 @@ function handleStart() {
   });
 
   Promise.allSettled(promises).then(data => {
+    const isWinner =
+      data.every(item => item.status === 'fulfilled') ||
+      data.every(item => item.status === 'rejected');
+
     data.forEach((item, i) => {
+      container.children[i].textContent = '';
+
       setTimeout(() => {
         container.children[i].textContent = item.value || item.reason;
+
+        if (i === data.length - 1) {
+          result.innerHTML = isWinner ? 'Winner' : 'Loser';
+          startBtn.disabled = false;
+        }
       }, 1000 * (i + 1));
     });
   });
